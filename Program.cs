933d3b811e4e51f3c8501ee3dddca9b76a0bdb6f
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-03-25 11:46:15 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string frasedigitada, frasealterada;
+Console.WriteLine("Digite uma frase: ");
+
+frasedigitada = Console.ReadLine()!;
+frasealterada = frasedigitada
+.Replace ("a", "AAAA")
+.Replace ("e", "EEEE")
+.Replace ("i", "IIII")
+.Replace ("o", "OOOO")
+.Replace ("u", "UUUU");
+
+Console.WriteLine($"Em baleiês: {frasealterada}");
